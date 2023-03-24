024a474b411b67269587387be1e6b894ff00d230
Quentin Aslan <quentin.aslan@outlook.com>
2023-03-24 02:02:40 -0400

    ✨Create PhoneChat component, display a chat inside a Phone Mockup.

--- components/Chat.tsx
+++ components/Chat.tsx
@@ -1,9 +1,6 @@
 import {useEffect, useState} from "react";
-
-type Message = {
-    message: string
-    isMe: boolean
-}
+import PhoneChat from "@/components/PhoneChat";
+import {Message} from "@/types/Message";
 
 export default function Chat () {
     const generateRandomSentence = () => {
@@ -21,7 +18,6 @@ export default function Chat () {
         setMessages(messages)
     }
 
-    const [message, setMessage] = useState('')
     const [messages, setMessages] = useState<Message[]>([])
 
     // Call generateFakeMessages when the component is mounted
@@ -29,38 +25,12 @@ export default function Chat () {
         generateFakeMessages()
     }, [])
 
-    const handleSendMessage = async () => {
+    const handleSendMessage = async (message: string) => {
         if (message.length === 0) return
         setMessages([...messages, { message, isMe: true }])
-        setMessage('')
     }
 
     return (
-        <>
-            <div className="card max-w-md card-bordered shadow-xl">
-                <div className="card-body">
-                    <div className="mockup-phone shadow-xl border-primary">
-                        <div className="camera"></div>
-                        <div className="display">
-                            <div className="artboard phone-1 mt-5 mb-5 flex flex-col-reverse space-y-4 space-y-reverse overflow-auto">
-                                <div className="flex space-x-2 justify-center items-center">
-                                    <div className="form-control">
-                                        <input value={message} onChange={e => setMessage(e.target.value)} onKeyDown={e => (e.key === 'Enter') ? handleSendMessage() : undefined} type="text" placeholder="Type a message" className="input input-bordered" />
-                                    </div>
-                                    <button onClick={handleSendMessage} className="btn btn-primary">Send</button>
-                                </div>
-                                <div className={"flex flex-col space-y-2"}>
-                                    {messages.map(message => (
-                                        <div className={`chat ${(message.isMe) ? 'chat-end' : 'chat-start'}`}>
-                                            <div className={`chat-bubble ${(message.isMe) ? 'chat-bubble-secondary' : 'chat-bubble-primary'}`}>{message.message}</div>
-                                        </div>
-                                    ))}
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </>
+        <PhoneChat title={"Fake Chat"} messages={messages} onSendMessage={handleSendMessage} />
     )
 }
---
+++ components/PhoneChat.tsx
@@ -0,0 +1,52 @@
+import {useEffect, useState} from "react";
+import {Message} from "@/types/Message";
+type Props = {
+    title?: string,
+    messages: Message[],
+    onSendMessage: (message: string) => void,
+}
+
+export default function PhoneChat (props: Props) {
+    const [message, setMessage] = useState('')
+
+    const handleSendMessage = async () => {
+        props.onSendMessage(message)
+        setMessage('')
+    }
+
+    return (
+        <>
+            <div className="card max-w-md card-bordered shadow-xl">
+                <div className="card-body">
+
+                    {props.title && (
+                        <div className="flex justify-center items-center">
+                            <h2 className="card-title">{props.title}</h2>
+                        </div>
+                    )}
+
+                    <div className="mockup-phone shadow-xl border-primary">
+                        <div className="camera"></div>
+                        <div className="display">
+                            <div className="artboard phone-1 mt-5 mb-5 flex flex-col-reverse space-y-4 space-y-reverse overflow-auto">
+                                <div className="flex space-x-2 justify-center items-center">
+                                    <div className="form-control">
+                                        <input value={message} onChange={e => setMessage(e.target.value)} onKeyDown={e => (e.key === 'Enter') ? handleSendMessage() : undefined} type="text" placeholder="Type a message" className="input input-bordered" />
+                                    </div>
+                                    <button onClick={handleSendMessage} className="btn btn-primary">Send</button>
+                                </div>
+                                <div className={"flex flex-col space-y-2"}>
+                                    {props.messages.map(message => (
+                                        <div className={`chat ${(message.isMe) ? 'chat-end' : 'chat-start'}`}>
+                                            <div className={`chat-bubble ${(message.isMe) ? 'chat-bubble-secondary' : 'chat-bubble-primary'}`}>{message.message}</div>
+                                        </div>
+                                    ))}
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
